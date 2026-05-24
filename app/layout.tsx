a6ea52b0528049c3ca739b2f331acf9aa6a7e430
Albert Cheng <acheng759@gmail.com>
2026-05-23 20:17:46 -0700

    setup

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+export const metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard built with App Router.',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en'>
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
