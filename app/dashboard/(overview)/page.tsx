6cb095e33f66e664c0c4fefacebabde2dc584d0d
Albert Cheng <acheng759@gmail.com>
2026-05-24 14:26:35 -0700

    refactor Cards with CardsWrapper component

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,17 +1,14 @@
-import { Card } from '@/app/ui/dashboard/cards';
+// import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import {
-  //  fetchRevenue,
-  // fetchLatestInvoices,
-  fetchCardData,
-} from '@/app/lib/data';
 import { Suspense } from 'react';
 import {
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
+  CardsSkeleton,
 } from '@/app/ui/skeletons';
+import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
 
 export default async function Page() {
   // NOTE: Uncomment this code in Chapter 7
@@ -19,12 +16,6 @@ export default async function Page() {
   /* const revenue = await fetchRevenue();    // moved into component */
   // const latestInvoices = await fetchLatestInvoices();
   // destructure the data returned from fetchCardData into individual variables
-  const {
-    numberOfInvoices,
-    totalPaidInvoices,
-    totalPendingInvoices,
-    numberOfCustomers,
-  } = await fetchCardData();
 
   return (
     <main>
@@ -32,22 +23,9 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
-        {<Card title='Collected' value={totalPaidInvoices} type='collected' />}
-        {<Card title='Pending' value={totalPendingInvoices} type='pending' />}
-        {
-          <Card
-            title='Total Invoices'
-            value={numberOfInvoices}
-            type='invoices'
-          />
-        }
-        {
-          <Card
-            title='Total Customers'
-            value={numberOfCustomers}
-            type='customers'
-          />
-        }
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
         <Suspense fallback={<RevenueChartSkeleton />}>
