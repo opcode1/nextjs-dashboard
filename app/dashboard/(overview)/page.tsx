274f57a70cb92918f8b6a5cde6b21682435f53ae
Albert Cheng <acheng759@gmail.com>
2026-05-24 13:03:38 -0700

    refactor LatestInvoices component for streaming

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,18 +3,21 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import {
-  //  fetchRevenue
-  fetchLatestInvoices,
+  //  fetchRevenue,
+  // fetchLatestInvoices,
   fetchCardData,
 } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import {
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from '@/app/ui/skeletons';
 
 export default async function Page() {
   // NOTE: Uncomment this code in Chapter 7
   // This triggers a data fetch
-  /* const revenue = await fetchRevenue();   // moved into component */
-  const latestInvoices = await fetchLatestInvoices();
+  /* const revenue = await fetchRevenue();    // moved into component */
+  // const latestInvoices = await fetchLatestInvoices();
   // destructure the data returned from fetchCardData into individual variables
   const {
     numberOfInvoices,
@@ -50,7 +53,9 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        {<LatestInvoices latestInvoices={latestInvoices} />}
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
